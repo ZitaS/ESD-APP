16b56d1b3b1a6d55169a4bf8563f09afacb93be5
Zita <zita.szentimrey@gmail.com>
2018-04-18 16:13:16 +0200

    Nouvelles pages

--- ESD-APP/src/app/app.module.ts
+++ ESD-APP/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ProfilPage } from '../pages/profil/profil';
+import { InscriptionPage } from '../pages/inscription/inscription';
+import { ConnexionPage } from '../pages/connexion/connexion';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +20,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ProfilPage,
+    InscriptionPage,
+    ConnexionPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +35,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ProfilPage,
+    InscriptionPage,
+    ConnexionPage
+    
   ],
   providers: [
     StatusBar,
--- ESD-APP/src/pages/tabs/tabs.ts
+++ ESD-APP/src/pages/tabs/tabs.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ProfilPage } from '../profil/profil';
+import { InscriptionPage } from '../inscription/inscription';
+import { ConnexionPage } from '../connexion/connexion';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +15,10 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = ProfilPage;
+  tab5Root = InscriptionPage;
+  tab6Root = ConnexionPage;
+  
 
   constructor() {
 
