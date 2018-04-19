ae361c7cf1a91c5dafcf8019456131185bdfd926
lailatamani <lailatamani@hotmail.com>
2018-04-19 16:32:11 +0200

    brha

---
+++ ESD-APP/src/app/app.firebase.config.ts
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
+<script>
+ // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyDC7f5NeraYvm7U21Qb041a0v0trEmgWM8",
+   authDomain: "zita-9c70e.firebaseapp.com",
+   databaseURL: "https://zita-9c70e.firebaseio.com",
+   projectId: "zita-9c70e",
+   storageBucket: "zita-9c70e.appspot.com",
+   messagingSenderId: "142471210208"
+ };
+ firebase.initializeApp(config);
+</script>
